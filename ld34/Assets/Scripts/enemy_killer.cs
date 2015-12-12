3d7560091ef3f07a0104eb3f4c473e30bf1320b7
JoronTheWise <Jthorpe94@hotmail.co.uk>
2015-12-12 20:52:42 +0000

    Dead Enemies

---
+++ ld34/Assets/Scripts/enemy_killer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemy_killer : MonoBehaviour {
+
+    // Use this for initialization
+    void Start() {
+        
+    }
+	
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "PlatformOfDeath")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
