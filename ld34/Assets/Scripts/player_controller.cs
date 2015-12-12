563291c0cc9c40fbe9b497f9f77c75b199c1a733
JoronTheWise <Jthorpe94@hotmail.co.uk>
2015-12-12 13:57:42 +0000

    bleurgh

--- ld34/Assets/Scripts/player_controller.cs
+++ ld34/Assets/Scripts/player_controller.cs
@@ -3,13 +3,20 @@ using System.Collections;
 
 public class player_controller : MonoBehaviour {
 
+	public float speed = 1f;
+	private float move = 1f;
+
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update () 
+	{
+		move = Input.GetAxis ("Horizontal");
+		GetComponent<Rigidbody2D>().velocity = new Vector2(move * speed, GetComponent<Rigidbody2D>().velocity.y);
 	}
+
 }
