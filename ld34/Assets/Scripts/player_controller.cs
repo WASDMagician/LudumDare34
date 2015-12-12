7aefbd384d20da93f8559fd3bf8f8217fe393d0b
JoronTheWise <Jthorpe94@hotmail.co.uk>
2015-12-12 18:04:54 +0000

    mh

--- ld34/Assets/Scripts/player_controller.cs
+++ ld34/Assets/Scripts/player_controller.cs
@@ -20,6 +20,7 @@ public class player_controller : MonoBehaviour {
 	// Update is called once per frame
 	void Update () 
 	{
+        
 		if(Input.GetKeyDown(KeyCode.W))
 		{
 			GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpHeight);
